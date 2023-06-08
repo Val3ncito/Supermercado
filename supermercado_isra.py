7ddc66ffc5da5a215da46a3dca20cfff5f615e7e
ialmanza <110355705+ialmanza@users.noreply.github.com>
2023-06-08 20:23:51 -0300

    Update supermercado_isra.py

--- supermercado_isra.py
+++ supermercado_isra.py
@@ -1,4 +1,21 @@
 #TRABAJO FINAL SUPERMERCADO
 
 produtos={}
-print("nuevo")
+
+
+
+def ingresarNuevoProducto():
+    id_producto = input("Ingrese el identificador del nuevo producto")
+    valor = []
+
+    codigo = int(input("Ingrese el codigo del producto")
+    descripcion = input("Ingrese ")
+    stock
+    precio
+unitario, fecha de vencimiento y tipo de producto, por ejemplo: "L" para lácteos, "V" para
+verdulería
+    
+    productos[id_producto] = 
+    
+    
+    
