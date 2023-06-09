6ff705cd6f472ab06aec8e8406c00d346370df83
ialmanza <110355705+ialmanza@users.noreply.github.com>
2023-06-08 21:21:11 -0300

    Update supermercado_isra.py
    trabajando con el mostrar productos

--- supermercado_isra.py
+++ supermercado_isra.py
@@ -2,26 +2,40 @@
 
 productos={}
 
+def mostrar():
+    claves = productos.keys()
+    for c in claves:
+        dato = productos[c]
+        print (dato[1],dato[2])
 
 
 def ingresarNuevoProducto():
-    id_producto = input("Ingrese el identificador del nuevo producto")
+    id_producto = input("Ingrese el identificador del nuevo producto: ")
     valor = []
+    almacen = {"V":"verduras","L":"lacteos","B": "bebidas","P":"panadería", "C":"carnes"}
+    
 
-    codigo = int(input("Ingrese el codigo del producto"))
-    descripcion = input("Ingrese descripción del producto")
-    stock = int(input("Ingrese la cantidad de producto"))
-    precio_unitario = float(input("Ingrese el precio del producto"))
+    codigo = int(input("Ingrese el codigo del producto: "))
+    descripcion = input("Ingrese descripción del producto: ")
+    stock = int(input("Ingrese la cantidad de producto: "))
+    precio_unitario = float(input("Ingrese el precio del producto: "))
+    tipo_de_producto = "verdura"
+    fecha_de_vencimiento = "5/1272023"
+    
+    
 
     valor.append(codigo)
     valor.append(descripcion)
     valor.append(stock)
     valor.append(precio_unitario)
+    valor.append(tipo_de_producto)
+    valor.append(fecha_de_vencimiento)
 
     
     productos[id_producto] = valor
 
 ingresarNuevoProducto()
+mostrar()
 print(productos)
     
     
