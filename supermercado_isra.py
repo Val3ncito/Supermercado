76edb7249688e672fba6f61a487c922e2c52d6aa
ialmanza <110355705+ialmanza@users.noreply.github.com>
2023-06-13 19:46:39 -0300

    Update supermercado_isra.py
    Realizados el listar, el añadir y el eliminar

--- supermercado_isra.py
+++ supermercado_isra.py
@@ -2,22 +2,21 @@
 
 productos={}
 
-def mostrarProductos():
+def listarProductos():
     
     claves = productos.keys()
     for c in claves:
         dato = productos[c]
-        print (c,dato[0],dato[1],dato[2],dato[3],dato[4],dato[5])
+        print (c,dato[0],dato[1],dato[2],dato[3],dato[4])
 
 
 def ingresarNuevoProducto():
     
-    id_producto = input("Ingrese el identificador del nuevo producto: ")
+    codigo = int(input("Ingrese el código del nuevo producto: "))
     valor = []
-    almacen = {"V":"verduras","L":"lacteos","B": "bebidas","P":"panadería", "C":"carnes"}
-    
+    #almacen = {"V":"verduras","L":"lacteos","B": "bebidas","P":"panadería", "C":"carnes"}
 
-    codigo = int(input("Ingrese el codigo del producto: "))
+    
     descripcion = input("Ingrese descripción del producto: ")
     stock = int(input("Ingrese la cantidad de producto: "))
     precio_unitario = float(input("Ingrese el precio del producto: "))
@@ -25,8 +24,6 @@ def ingresarNuevoProducto():
     fecha_de_vencimiento = "5/12/2023"
     
     
-
-    valor.append(codigo)
     valor.append(descripcion)
     valor.append(stock)
     valor.append(precio_unitario)
@@ -34,7 +31,7 @@ def ingresarNuevoProducto():
     valor.append(fecha_de_vencimiento)
 
     
-    productos[id_producto] = valor
+    productos[codigo] = valor
 
     
     print("Su producto fue añadido satisfactoriamente")
@@ -44,7 +41,7 @@ def ingresarNuevoProducto():
     if nuevo_producto == 1:
         ingresarNuevoProducto()
     else:
-        mostrarProductos()
+        listarProductos()
         
 def ingresarProductoExistente(diccionario, id):
     for clave, valor in diccionario.items():
@@ -58,38 +55,19 @@ def ingresarProductoExistente(diccionario, id):
         
             
             
-def eliminarProducto(diccionario,codigo_producto):
+def eliminarProducto(diccionario):
+    codigo = int(input("Ingrese el código del producto que desea a eliminar: "))
+    del diccionario[codigo]
+    print("El producto ha sido eliminado satisfactoriamente")
     
-    lista_claves = list(diccionario.keys())
-    lista_valores = list(diccionario.values())
-    
-    for i in lista_valores:
-        
-        """recorriendo todos los valores del diccionario"""
-        
-        for j in i:
-            
-            """ recorriendo todos los datos dentro de la lista de cada uno de los valores"""
-
-            if codigo_producto == j:
-                """  ya tengo el codigo del producto que quiero eliminar guardado en esa variable"""
-                
-                
-                posicion = lista_valores.index(i)
-                del lista_claves[posicion]
-
-                """ devuelve la posicion en la que se encuentra el codigo del producto a eliminar y esta guardado en la variable posision"""
-    print(lista_claves)            
-    """ me equivoque y elimina la clave en la lista no en el diccionario"""   
-    mostrarProductos()    
 
 
     
 
 ingresarNuevoProducto()
-#mostrarProductos()
+
 #id_producto= int(input("Ingrese el id del producto existente: "))
 #ingresarProductoExistente(productos,id_producto)
-eliminarProducto(productos,56)
+eliminarProducto(productos)
 
     
