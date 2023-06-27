172d6c5f4432de424f4a23e8210f6719097be5e0
Valencito <macinellivalentino@gmail.com>
2023-06-27 03:54:36 -0300

    Update Supermercado Valen.py

--- Supermercado Valen.py
+++ Supermercado Valen.py
@@ -4,22 +4,20 @@
 ✔Eliminar un producto dado su código.
 ✔Listar todos los productos de una forma prolija.
 ✔ Actualizar el stock cuando se vende un producto.
-• Actualizar el precio unitario de un producto determinado en un cierto procentaje.
+✔ Actualizar el precio unitario de un producto determinado en un cierto porcentaje.
 ✔Determinar la existencia de un producto para poder vender la cantidad solicitada.
-• Reponer un producto cuando el stock está por debajo de un mínimo requerido.
+✔ Reponer un producto cuando el stock está por debajo de un mínimo requerido.
 ✔ Pedir los datos de un cliente para hacer envío a domicilio.
-• Determinar cuál es el artículo más vendido.
-• Eliminar del supermercado (guardarlos en un otro diccionario) los artículos que estén
-vencidos.
-•- Simular la venta a un cliente y emitir el ticket de venta.
-✔ Agregar información adicional al producto para saber si un determinado producto tiene o no
-descuento.
-•- Si el producto vence en una semana hacer un 10% de descuento.
-• Determinar el producto más vendido dependiendo del tipo de producto.
+X Determinar cuál es el artículo más vendido.
+•- Eliminar del supermercado (guardarlos en un otro diccionario) los artículos que estén vencidos.
+X Simular la venta a un cliente y emitir el ticket de venta.
+✔ Agregar información adicional al producto para saber si un determinado producto tiene o no descuento.
+X Si el producto vence en una semana hacer un 10% de descuento.
+X Determinar el producto más vendido dependiendo del tipo de producto.
 """
 
-
-productos={"1234":["Lechuga", "0001", "50", "Verdura", "5/12/2023", "10"],"5678":["Tomate", "0002", "75", "Verdura", "7/12/2023]"]}
+from datetime import datetime
+productos={"1234":["Lechuga", "0001", "50", "Verdura", "2023/8/30", "10"],"5678":["Tomate", "0002", "4000", "Verdura", "2023-07-12]"]}
 Usuarios={"123":["Valen", "Roque Saenz Peña"], "456":["Pepe", "Cabildo"]}
 
 
@@ -42,6 +40,7 @@ def ingresarNuevoProducto():
     stock = int(input("Ingrese el stock de producto: "))
     precio_unitario = float(input("Ingrese el precio del producto: "))
     tipo_de_producto = "verdura"
+    from datetime import datetime
     fecha_de_vencimiento = "5/12/2023"
     
     
@@ -94,30 +93,13 @@ def determinarExistenciaDelProducto(diccionario):
             print("El producto no existe")
 
 def reponerProducto(diccionario):
-    
-    
     minimo = 10
 
-    lista_claves = list(diccionario.keys())
-    lista_valores = list(diccionario.values())
-    
-    for i in lista_valores:
-        
-        """recorriendo todos los valores del diccionario"""
-        
-        for j in i:
-            
-            """ recorriendo todos los datos dentro de la lista de cada uno de los valores"""
-
-            if stock_producto == j:
-                """  ya tengo el codigo del producto que quiero eliminar guardado en esa variable"""
-                
-                
-                posicion = lista_valores.index(i)
-                del lista_claves[posicion]
-
-                """ devuelve la posicion en la que se encuentra el codigo del producto a eliminar y esta guardado en la variable posision"""
-
+    for i in diccionario:
+        if diccionario[i][1]== 0:
+            print("Producto sin stock")
+        if diccionario[i][1] < minimo:
+            print("Alerta!! Reponer stock")
 
 
 def EnvioDomicilio(Usuarios):
@@ -144,6 +126,7 @@ def EnvioDomicilio(Usuarios):
 
 
 def Descuento(productos):
+    #FuncionISra
     Codigo=input("Ingresar Código del Producto: ")
     if len(productos.get(Codigo))==5:
         print("El producto no tiene descuento")
@@ -154,36 +137,13 @@ def Descuento(productos):
 
 
 
-def DescuentoVencimiento(productos):
-    for i in productos.keys():
-        A=len(productos.get(i))
-        if A==5:
-            if productos.get(i)[4]
-            
-        elif A==!5:
-            
 
-DescuentoVencimiento(productos)
-  
-
-
-"""
-def VentaCliente(Productos):
-    Medio=input("Ingresar medio de pago 1-Efectivo   2-Tarjeta de crédito/débito: ")
-    if Medio==1:
-        Dinero=int(input("Ingresar Cantidad de dinero: "))
-       # if Descuento==True:
-       #     Dinero=Dinero%10:
-        Vuelto=Dinero
-        
-    elif Medio==2:
-
-    else:
-        print("Se canceló el pago")
- 
-"""    
-    
-
+def ActualizarPrecio(productos):
+    Clave=input("Ingresar Código de Producto: ")
+    Porcentaje=int(input("Ingresar Porcentaje: "))
+    Precio=int((productos.get(Clave)[2]))
+    PrecioF=(Precio+(Precio*Porcentaje/100))
+    productos.get(Clave)[2]=PrecioF
 
 
 
