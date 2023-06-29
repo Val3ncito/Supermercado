8e649f70171fd9ac2f090e9e124dbdf0a2524cee
Valencito <macinellivalentino@gmail.com>
2023-06-28 23:08:29 -0300

    Update Supermercado Valen.py
    Solo estoy poniendo todo prolijo

--- Supermercado Valen.py
+++ Supermercado Valen.py
@@ -1,107 +1,86 @@
-#TRABAJO FINAL SUPERMERCADO
 """
-✔Agregar un nuevo producto.
-✔Eliminar un producto dado su código.
-✔Listar todos los productos de una forma prolija.
-✔ Actualizar el stock cuando se vende un producto.
-✔ Actualizar el precio unitario de un producto determinado en un cierto porcentaje.
-✔Determinar la existencia de un producto para poder vender la cantidad solicitada.
-✔ Reponer un producto cuando el stock está por debajo de un mínimo requerido.
-✔ Pedir los datos de un cliente para hacer envío a domicilio.
-X Determinar cuál es el artículo más vendido.
-•- Eliminar del supermercado (guardarlos en un otro diccionario) los artículos que estén vencidos.
-X Simular la venta a un cliente y emitir el ticket de venta.
-✔ Agregar información adicional al producto para saber si un determinado producto tiene o no descuento.
-X Si el producto vence en una semana hacer un 10% de descuento.
-X Determinar el producto más vendido dependiendo del tipo de producto.
+1✔Agregar un nuevo producto.
+2✔ Eliminar un producto dado su código.
+3✔ Listar todos los productos de una forma prolija.
+4✔ Actualizar el stock cuando se vende un producto.
+5✔ Actualizar el precio unitario de un producto determinado en un cierto porcentaje.
+6✔ Determinar la existencia de un producto para poder vender la cantidad solicitada.
+7✔ Reponer un producto cuando el stock está por debajo de un mínimo requerido.
+8✔ Pedir los datos de un cliente para hacer envío a domicilio.
+9X Determinar cuál es el artículo más vendido.
+10X Eliminar del supermercado (guardarlos en un otro diccionario) los artículos que estén vencidos.
+11X Simular la venta a un cliente y emitir el ticket de venta.
+12✔ Agregar información adicional al producto para saber si un determinado producto tiene o no descuento.
+13✔ Si el producto vence en una semana hacer un 10% de descuento.
+14X Determinar el producto más vendido dependiendo del tipo de producto.
 """
 
 from datetime import datetime
 productos={"1234":["Lechuga", "0001", "50", "Verdura", "2023/8/30", "10"],"5678":["Tomate", "0002", "4000", "Verdura", "2023-07-12]"]}
 Usuarios={"123":["Valen", "Roque Saenz Peña"], "456":["Pepe", "Cabildo"]}
 
-
-def listarProductos():
-    
-    claves = productos.keys()
-    for c in claves:
-        dato = productos[c]
-        print (c,dato[0],dato[1],dato[2],dato[3],dato[4])
-
-
-def ingresarNuevoProducto():
-    
-    codigo = int(input("Ingrese el código del nuevo producto: "))
-    valor = []
-    #almacen = {"V":"verduras","L":"lacteos","B": "bebidas","P":"panadería", "C":"carnes"}
-
-    
-    descripcion = input("Ingrese descripción del producto: ")
-    stock = int(input("Ingrese el stock de producto: "))
-    precio_unitario = float(input("Ingrese el precio del producto: "))
-    tipo_de_producto = "verdura"
-    from datetime import datetime
-    fecha_de_vencimiento = "5/12/2023"
-    
-    
-    valor.append(descripcion)
-    valor.append(stock)
-    valor.append(precio_unitario)
-    valor.append(tipo_de_producto)
-    valor.append(fecha_de_vencimiento)
-
-    
-    productos[codigo]=valor
-
-    
-    print("Su producto fue añadido satisfactoriamente")
-
-    nuevo_producto = int(input("Ingrese 1 para añadir nuevo producto ó 2 para salir: "))
-
-    if nuevo_producto == 1:
-        ingresarNuevoProducto()
-    else:
-        listarProductos()
+#1
+def IngresarNuevoProducto():
+    valor=[]
+    codigo=int(input("Ingrese el código del nuevo producto: "))
+    descripcion=input("Ingrese descripción del producto: ")
+    stock=int(input("Ingrese el stock de producto: "))
+    precio_unitario=float(input("Ingrese el precio del producto: "))
+    tipo_de_producto="verdura"
+    fecha_de_vencimiento=input("Ingrese fecha de vencimiento dd/mm/aa: ")
+    fecha = datetime.strptime(fecha_de_vencimiento, "%d/%m/%Y")
+    tiene_descuento = False
         
-##def ingresarProductoExistente(diccionario, id):
-##    for clave, valor in diccionario.items():
-##        if clave == id:
-##            
-##            print(f"El valor para la clave {clave} es {valor}.")
-##            break
-##    else:
-##        
-##        print("No se encontró ningún ID que coincida con el ID proporcionado.")
-##        
-##            
-            
-def eliminarProducto(diccionario):
+#2          
+def EliminarProducto(diccionario):
     codigo = int(input("Ingrese el código del producto que desea a eliminar: "))
     del diccionario[codigo]
     print("El producto ha sido eliminado satisfactoriamente")
 
+#3
+def ListarProductos():
+    claves=productos.keys()
+    for c in claves:
+        dato=productos[c]
+        print(c,dato[0],dato[1],dato[2],dato[3],dato[4])
 
-def determinarExistenciaDelProducto(diccionario):
+#4
+def ActualizarStock(diccionario):
+    descripcion = input("Ingrese descripción del producto que desea comprar: ")
+    stock = int(input("Ingrese la cantidad deseada: "))
+    for i in diccionario:
+        if diccionario[i][0] == descripcion:
+            stock_real = diccionario[i][1] - stock
+            print("El stock real del producto es " + str(stock_real))
+
+#5
+def ActualizarPrecio(productos):
+    Clave=input("Ingresar Código de Producto: ")
+    Porcentaje=int(input("Ingresar Porcentaje: "))
+    Precio=int((productos.get(Clave)[2]))
+    PrecioF=(Precio+(Precio*Porcentaje/100))
+    productos.get(Clave)[2]=PrecioF
+
+#6
+def DeterminarExistenciaDelProducto(diccionario):
     codigo = int(input("Ingrese el código del producto del cual desea saber su existencia: "))
-
     claves = diccionario.keys()
-
     for i in claves:
         if i == codigo:
             listarProductos()
         else:
             print("El producto no existe")
 
-def reponerProducto(diccionario):
+#7
+def ReponerProducto(diccionario):
     minimo = 10
-
     for i in diccionario:
         if diccionario[i][1]== 0:
             print("Producto sin stock")
         if diccionario[i][1] < minimo:
             print("Alerta!! Reponer stock")
 
-
+#8
 def EnvioDomicilio(Usuarios):
     DNI=input("Ingresar DNI: ")
     valor=[]
@@ -123,27 +102,41 @@ def EnvioDomicilio(Usuarios):
         print("Domicilio: ", Usuarios[DNI][1])
         print("A pedido de: ",Usuarios[DNI][0])
 
+#12
+def TieneDescuento(diccionario):
+    codigo = int(input("Ingrese el código del producto del cual desea saber si tiene descuento: "))
+    claves = diccionario.keys()
+    for i in claves:
+        if i == codigo:
+            aux = ""
+            if diccionario[i][5]:
+                aux = " El producto tiene descuento"
+                print(aux)
+            else:
+                aux = "El producto no tiene descuento"
+                print(aux)
+
+#13
+def DescuentoProducto(diccionario):
+    fecha_actual = datetime.now()
+    for i in diccionario:
+        fecha_vencimiento = diccionario[i][4]
+        prueba = fecha_vencimiento - fecha_actual
+        
+        if  int(prueba.days)<= 7 and (prueba.days)>0:
+            precio = diccionario[i][2]
+            descuento = precio * 0.1
+            diccionario[i][2] = precio - descuento
+            diccionario[i][5] = True
 
 
-def Descuento(productos):
-    #FuncionISra
-    Codigo=input("Ingresar Código del Producto: ")
-    if len(productos.get(Codigo))==5:
-        print("El producto no tiene descuento")
-    elif len(productos.get(Codigo))!=5:
-        print("El producto tiene un descuento del "+str(productos.get(Codigo)[5])+"%")
-        Precio=int((int(productos.get(Codigo)[2]))*(int(productos.get(Codigo)[5])/100))
-        print("Precio: "+str(Precio)+"$")
+#9
+#10
+#11
+#14
 
 
 
-
-def ActualizarPrecio(productos):
-    Clave=input("Ingresar Código de Producto: ")
-    Porcentaje=int(input("Ingresar Porcentaje: "))
-    Precio=int((productos.get(Clave)[2]))
-    PrecioF=(Precio+(Precio*Porcentaje/100))
-    productos.get(Clave)[2]=PrecioF
 
 
 
