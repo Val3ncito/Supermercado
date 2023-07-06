1870e1608b3d6fb643bf718f72f4628a67cc6f0e
Valencito <macinellivalentino@gmail.com>
2023-07-06 18:53:39 -0300

    Update supermercado_isra.py

--- supermercado_isra.py
+++ supermercado_isra.py
@@ -4,18 +4,18 @@
 ✔Eliminar un producto dado su código.
 ✔Listar todos los productos de una forma prolija.
 ✔Actualizar el stock cuando se vende un producto.
-✔ Actualizar el precio unitario de un producto determinado en un cierto procentaje.
+✔Actualizar el precio unitario de un producto determinado en un cierto procentaje.
 ✔Determinar la existencia de un producto para poder vender la cantidad solicitada.
 ✔Reponer un producto cuando el stock está por debajo de un mínimo requerido.
 ✔Pedir los datos de un cliente para hacer envío a domicilio.
-✔ Determinar cuál es el artículo más vendido.
+✔Determinar cuál es el artículo más vendido.
 ✔Eliminar del supermercado (guardarlos en un otro diccionario) los artículos que estén
 vencidos.
-• Simular la venta a un cliente y emitir el ticket de venta.
+✔Simular la venta a un cliente y emitir el ticket de venta.
 ✔Agregar información adicional al producto para saber si un determinado producto tiene o no
 descuento.
 ✔Si el producto vence en una semana hacer un 10% de descuento.
-• Determinar el producto más vendido dependiendo del tipo de producto.
+✔Determinar el producto más vendido dependiendo del tipo de producto.
 """
 
 
